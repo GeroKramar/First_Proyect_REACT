8c03e1c278821f8837ade32bf23bcc20816d2ba4
Geronimo Kramar <132961854+GeroKramar@users.noreply.github.com>
2023-11-02 21:41:18 -0300

    select-player

--- First_Proyect_React/src/App.jsx
+++ First_Proyect_React/src/App.jsx
@@ -1,13 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
+const TURNS = {
+  X: 'X',
+  O: 'O',
+}
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+  const handleClick = () => {
+    updateBoard()
+  }
+  return (
+    <div onClick={updateBoard} className={className}>
+      {children}
+    </div>
+  )
+}
+
 
 function App() {
- 
+  const [board, setBoard] = useState(Array(9).fill(null ))
+  const [turn, setTurn] = useState(TURNS.X)
+  const updateBoard = () => {
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
 
   return (
-    <h1>TIC TAC TOE</h1>
+    <main className='board'>
+      <h1>TIC TAC TOE</h1>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+
+              </Square>
+            )
+          })
+        }
+
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+    </main>
   )
 }
 
